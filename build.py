75af060f10d02755d10e0ff930c6e7c31770a1c4
Twinkle <67370801+printf-twinkle@users.noreply.github.com>
2020-10-17 18:11:21 +0530

    This is the updated code

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
-num=int(input("Enter a number:"))
-temp=num
-rev=0
-while(num>0):
-    dig=num%10
-    rev=rev*10+dig
-    num=num//10
-if(temp==rev):
-    print("The number is palindrome!")
-else:
-    print("Not a palindrome!")
+#function solution takes list as parameter
+def solution(lst):
+	#string in list is reversed
+	rev=lst[::-1]
+	print("Reversed list is:\t",rev)
+	#reversed string is checked with orginal string in list
+	if(rev==lst):print("Thus the list is palindrome!\n");
+	else:print("Thus the list is not palindrome!\n");
+
+
+s=input("Enter the word:\n")
+l=[]
+for i in range(len(s)):
+	l.append((s[i:i+1]))
+print("Your initial list is:\t",l)
+solution(l)
